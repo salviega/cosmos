2326f111634cb1505ce0b6771ef161054b98ffd3
Santiago <salviega6@gmail.com>
2022-10-23 01:52:57 -0500

    [FEAT] Was added web3 to form

--- src/components/CosmosMaker/index.js
+++ src/components/CosmosMaker/index.js
@@ -3,36 +3,54 @@ import defaultImage from '../../asserts/images/default-image.jpg'
 import './CosmosMaker.scss'
 import { Navigate, useNavigate } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
+import addresses from "../../blockchain/environment/contract-address.json";
+import { v1 as uuid } from 'uuid' 
+import benefitsContractAbi from "../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/BenefitsContract.sol/BenefitsContract.json";
+import { ethers } from 'ethers';
+const cosmoContractAddress = addresses[1].cosmocontract;
+const benefitsContractAddress = addresses[3].benefitscontract;
+
 
 export function CosmosMaker({createItem, setSincronizedItems }) {
   const auth = useAuth()
   const navigate = useNavigate()
   const [imageBase64, setImageBase64] = React.useState('')
-  const site = React.useRef()
-  const city = React.useRef()
+  const maxNft = React.useRef()
+  const managerAddress = React.useRef()
+  const uri = React.useRef()
   const price = React.useRef()
-  const artist = React.useRef()
-  const address = React.useRef()
-  const capacity = React.useRef()
-  const doorOpening = React.useRef()
-  const responsible = React.useRef()
-  const minimunEntryAge = React.useRef()
-
-  const createEvent = async (event) => {
+  const name = React.useRef()
+  const symbol = React.useRef()
+  const description = React.useRef()
+  
+  const createBenefit = async (event) => {
     event.preventDefault();
+    const benefitId = uuid()
     const info = {
-      owner: auth.user.walletAddress, 
+      benefitId: benefitId,
+      maxNft: maxNft.current.value,
+      managerAddress: managerAddress.current.value,
+      uri: uri.current.value,
+      price: price.current.value,
+      cosmoContractAddress: cosmoContractAddress,
+      name: name.current.value, 
+      symbol: symbol.current.value,
       imageBase64: imageBase64,
-      artist: artist.current.value, 
-      site: site.current.value,
-      city: city.current.value,      
-      minimunEntryAge: minimunEntryAge.current.value,
-      responsible: responsible.current.value,
-      address: address.current.value,
-      doorOpening: doorOpening.current.value,
-      capacity: capacity.current.value,
-      price: price.current.value
+      owner: auth.user.walletAddress, 
+      description: description.current.value
     }
+
+    const web3Provider = new ethers.providers.Web3Provider(window.ethereum);
+    const web3Signer = web3Provider.getSigner();
+
+    const benefitsContract = new ethers.Contract(
+      benefitsContractAddress,
+      benefitsContractAbi.abi,
+      web3Signer
+    );
+
+    await benefitsContract.createBenefit(info.benefitId, info.maxNft, info.managerAddress, info.uri, info.price, info.cosmoContractAddress, info.name, info.symbol)
+
     await createItem(info)
     setSincronizedItems(false)
     alert('Event created')
@@ -59,7 +77,7 @@ export function CosmosMaker({createItem, setSincronizedItems }) {
   return (
     <div className="maker">
     <h1 className="maker__title">Create new event</h1>
-    <form className="maker-form" onSubmit={createEvent}>
+    <form className="maker-form" onSubmit={createBenefit}>
       <p className="maker-form__description">Click on the pictue to upgrade the image</p>
       <div className='maker-form-image'>
           <figure>
@@ -70,40 +88,32 @@ export function CosmosMaker({createItem, setSincronizedItems }) {
           </figure>
       </div>  
       <span>
-        <p className="maker-form__subtitle">Artist</p>
-        <input className="maker-form__add" ref={artist}/>
+        <p className="maker-form__subtitle">maxNft</p>
+        <input className="maker-form__add" ref={maxNft}/>
       </span>
       <span>
-        <p className="maker-form__subtitle">Site</p>
-        <input className="maker-form__add" ref={site}/>
+        <p className="maker-form__subtitle">managerAddress</p>
+        <input className="maker-form__add" ref={managerAddress}/>
       </span>
       <span>
-        <p className="maker-form__subtitle">City</p>
-        <input className="maker-form__add" ref={city}/>
+        <p className="maker-form__subtitle">uri</p>
+        <input className="maker-form__add" ref={uri}/>
       </span>
       <span>
-        <p className="maker-form__subtitle">Minimun entry age</p>
-        <input className="maker-form__add" ref={minimunEntryAge}/>
-      </span>
-      <span>
-        <p className="maker-form__subtitle">Responsible</p>
-        <input className="maker-form__add" ref={responsible}/>
-      </span>
-      <span>
-        <p className="maker-form__subtitle">Address</p>
-        <input className="maker-form__add" ref={address} />
-      </span>
-      <span>
-        <p className="maker-form__subtitle">Door opeing</p>
-        <input className="maker-form__add" ref={doorOpening}/>
-      </span>
-      <span>
-        <p className="maker-form__subtitle">Capacity</p>
-        <input className="maker-form__add" ref={capacity}/>
-      </span>
-      <span>
-        <p className="maker-form__subtitle">Price</p>
+        <p className="maker-form__subtitle">price</p>
         <input className="maker-form__add" ref={price}/>
+      </span>
+      <span>
+        <p className="maker-form__subtitle">name</p>
+        <input className="maker-form__add" ref={name}/>
+      </span>
+      <span>
+        <p className="maker-form__subtitle">symbol</p>
+        <input className="maker-form__add" ref={symbol}/>
+      </span>
+      <span>
+        <p className="maker-form__subtitle">description</p>
+        <input className="maker-form__add" ref={description}/>
       </span>
       <button className="maker-form__submit">Create</button>
     </form>
