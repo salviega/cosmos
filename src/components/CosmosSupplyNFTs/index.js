a2cfce1b73685da8cb7ea726b3e29578c7b90ac9
Santiago <salviega6@gmail.com>
2022-10-25 08:14:48 -0500

    [FEAT] Was changes in the form the supply component

--- src/components/CosmosSupplyNFTs/index.js
+++ src/components/CosmosSupplyNFTs/index.js
@@ -1,12 +1,10 @@
 import './CosmosSupplyNFTs.scss'
 import React from 'react'
 import { ethers } from 'ethers'
-import marketPlaceContractAbi from '../../blockchain/hardhat/artifacts/src/blockchain/hardhat/contracts/MarketplaceContract.sol/MarketPlaceContract.json'
-import addresses from '../../blockchain/environment/contract-address.json'
-const cosmoContractAddress = addresses[1].cosmocontract
-const marketPlaceContractAddress = addresses[2].marketplacecontract
+import { useContracts } from '../CosmosContext'
 
 export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedItems }) {
+  const contracts = useContracts()
   const price = React.useRef()
   const tokenURI = React.useRef()
   const tokenId = React.useRef()
@@ -15,41 +13,39 @@ export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedIt
 
   const putInSale = async (event) => {
     event.preventDefault()
-    const roundPrice = Math.round(parseInt(price.current.value))
+    let roundPrice = Math.round(parseInt(price.current.value))
+    let parsedTaxFee = parseInt(taxFee.current.value)
+    parsedTaxFee = (parsedTaxFee * roundPrice) / 100
+    parsedTaxFee = ethers.utils.parseEther(parsedTaxFee.toString(), 'ether')
+    roundPrice = ethers.utils.parseEther(roundPrice.toString(), 'ether')
     const parsedTokenId = parseInt(tokenId.current.value)
-    try {
-      const web3Provider = new ethers.providers.Web3Provider(window.ethereum)
-      const web3Signer = web3Provider.getSigner()
 
-      const marketPlaceContract = new ethers.Contract(
-        marketPlaceContractAddress,
-        marketPlaceContractAbi.abi,
-        web3Signer
-      )
-      const response = await marketPlaceContract.mint(
+    try {
+       
+      const response = await contracts.marketPlaceContract.mint(
         tokenURI.current.value,
         artistWallet.current.value,
-        taxFee.current.value,
-        cosmoContractAddress
+        parsedTaxFee,
+        contracts.cosmoContract.address
       )
       setLoading(true)
 
-      web3Provider
+      contracts.web3Provider
         .waitForTransaction(response.hash)
         .then(async (_response) => {
-          const response2 = await marketPlaceContract.approve(
-            marketPlaceContractAddress,
+          const response2 = await contracts.marketPlaceContract.approve(
+            contracts.marketPlaceContract.address,
             parsedTokenId
           )
-          web3Provider
+          contracts.web3Provider
             .waitForTransaction(response2.hash)
             .then(async (_response2) => {
-              const response3 = await marketPlaceContract.sellItem(
-                marketPlaceContractAddress,
+              const response3 = await contracts.marketPlaceContract.sellItem(
+                contracts.marketPlaceContract.address,
                 parsedTokenId,
                 roundPrice
               )
-              web3Provider
+              contracts.web3Provider
                 .waitForTransaction(response3.hash)
                 .then((_response3) => {
                   setTimeout(() => {
@@ -74,21 +70,20 @@ export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedIt
       console.error(error)
       setLoading(false)
     }
-  }
+   }
 
   return (
     <div className='supply'>
       <h1 className='supply__title'>Sell NFT</h1>
       <form className='supply-form' onSubmit={putInSale}>
           <span>
-            <p className='supply-form__subtitle'>Add the price in USD: </p>
+            <p className='supply-form__subtitle'>Add the price in COSMOS: </p>
             <input
               className='supply-form__add'
               type='number'
               required
               min='1'
               step='0.01'
-              placeholder='10'
               ref={price}
             />
           </span>
@@ -100,7 +95,6 @@ export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedIt
               required
               min='0'
               step='0'
-              placeholder='1'
               ref={tokenId}
             />
           </span>
@@ -110,7 +104,6 @@ export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedIt
               className='supply-form__add'
               type='url'
               required
-              placeholder='https://gateway/ipfs/example-metadata.json'
               ref={tokenURI}
             />
           </span>
@@ -120,19 +113,18 @@ export function CosmosSupplyNFTs ({ tokenIdCounter, setLoading, setSincronizedIt
               className='supply-form__add'
               type='text'
               required
-              placeholder='0x70.....BC685'
               ref={artistWallet}
             />
           </span>
           <span>
-            <p className='supply-form__subtitle'>Fee of the artist: </p>
+            <p className='supply-form__subtitle'>% Fee of the artist: </p>
             <input
               className='supply-form__add'
               type='number'
               required
               min='1'
-              step='0.01'
-              placeholder='1'
+              step='1'
+              max='100'
               ref={taxFee}
             />
           </span>
